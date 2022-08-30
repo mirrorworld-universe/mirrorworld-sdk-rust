d43f51d8ce3e15defb53fbb838bb79de8c8c660d
sunhonx <sunhonx@outlook.com>
2022-08-30 19:43:48 +0800

    feat: make func public

--- src/marketplace.rs
+++ src/marketplace.rs
@@ -26,179 +26,169 @@ pub struct Response<T> {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreateCollectionData {
-    mint_address: String,
-    url: String,
-    update_authority: String,
-    creator_address: String,
-    name: String,
-    symbol: String,
-    collection: Option<String>,
-    signature: String,
-    status: Option<String>,
+    pub mint_address: String,
+    pub url: String,
+    pub update_authority: String,
+    pub creator_address: String,
+    pub name: String,
+    pub symbol: String,
+    pub collection: Option<String>,
+    pub signature: String,
+    pub status: Option<String>,
 }
-
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CreateSubCollectionResp {
-    status: String,
-    data: Option<CreateCollectionData>,
-    code: u32,
-    message: Option<String>,
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GeneralPayload {
-    name: String,
-    symbol: String,
-    url: String,
-    collection_mint: String,
+   pub name: String,
+   pub symbol: String,
+   pub url: String,
+   pub collection_mint: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolanaMintNftResult {
-    mint_address: String,
-    url: String,
-    update_authority: String,
-    creator_address: String,
-    name: String,
-    symbol: String,
-    collection: String,
-    signature: String,
-    status: String,
+    pub mint_address: String,
+    pub url: String,
+    pub update_authority: String,
+    pub creator_address: String,
+    pub name: String,
+    pub symbol: String,
+    pub collection: String,
+    pub signature: String,
+    pub status: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct NftListing {
-    id: usize,
-    type_name: String,
-    wallet_address: String,
-    mint_address: String,
-    price: String,
-    seller_address: String,
-    to_wallet_address: String,
-    signature: String,
-    status: String,
-    update_at: String,
-    create_at: String,
+    pub id: usize,
+    pub type_name: String,
+    pub wallet_address: String,
+    pub mint_address: String,
+    pub price: String,
+    pub seller_address: String,
+    pub to_wallet_address: String,
+    pub signature: String,
+    pub status: String,
+    pub update_at: String,
+    pub create_at: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Creator {
-    address: String,
-    verified: bool,
-    share: f32,
+    pub address: String,
+    pub verified: bool,
+    pub share: f32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Owner {
-    address: String,
+    pub address: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MetadataAttribute {
-    trait_type: String,
-    value: String,
+    pub trait_type: String,
+    pub value: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolanaNFTListing {
-    id: usize,
+    pub id: usize,
     #[serde(rename = "tradeState")]
-    trade_state: String,
-    seller: String,
-    metadata: String,
+    pub trade_state: String,
+    pub seller: String,
+    pub metadata: String,
     #[serde(rename = "purchaseId")]
-    purchase_id: Option<String>,
-    price: f32,
+    pub purchase_id: Option<String>,
+    pub price: f32,
     #[serde(rename = "tokenSize")]
-    token_size: usize,
+    pub token_size: usize,
     #[serde(rename = "createdAt")]
-    created_at: String,
+    pub created_at: String,
     #[serde(rename = "canceledAt")]
-    canceled_at: Option<String>,
+    pub canceled_at: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolanaNFTExtended {
-    name: String,
+    pub name: String,
     #[serde(rename = "sellerFeeBasisPoints")]
-    seller_fee_basic_points: usize,
+    pub seller_fee_basic_points: usize,
     #[serde(rename = "updateAuthorityAddress")]
-    update_authority_address: String,
-    description: String,
-    image: String,
+    pub update_authority_address: String,
+    pub description: String,
+    pub image: String,
     #[serde(rename = "externalUrl")]
-    external_url: String,
-    creators: Vec<Creator>,
-    owner: Owner,
-    attributes: Vec<MetadataAttribute>,
-    listings: Vec<SolanaNFTListing>,
+    pub external_url: String,
+    pub creators: Vec<Creator>,
+    pub owner: Owner,
+    pub attributes: Vec<MetadataAttribute>,
+    pub listings: Vec<SolanaNFTListing>,
 }
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolanaNFTAuctionActivity {
-    id: usize,
+    pub id: usize,
     #[serde(rename = "mintAddress")]
-    mint_address: String,
+    pub mint_address: String,
     #[serde(rename = "txSignature")]
-    tx_signature: String,
-    amount: f32,
+    pub tx_signature: String,
+    pub amount: f32,
     #[serde(rename = "receiptType")]
-    receipt_type: String,
+    pub receipt_type: String,
     #[serde(rename = "tokenPrice")]
-    token_price: String,
+    pub token_price: String,
     #[serde(rename = "blockTimeCreated")]
-    block_time_created: String,
+    pub block_time_created: String,
     #[serde(rename = "blockTimeCanceled")]
-    block_time_canceled: Option<String>,
+    pub block_time_canceled: Option<String>,
     #[serde(rename = "tradeState")]
-    trade_state: String,
+    pub trade_state: String,
     #[serde(rename = "auctionHouseAddress")]
-    auction_house_address: String,
+    pub auction_house_address: String,
     #[serde(rename = "sellerAddress")]
-    seller_address: String,
+    pub seller_address: String,
     #[serde(rename = "buyerAddress")]
-    buyer_address: Option<String>,
-    metadata: String,
+    pub buyer_address: Option<String>,
+    pub metadata: String,
     #[serde(rename = "blockTime")]
-    block_time: String,
+    pub block_time: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolanaNFTTransfersEntity {
-    id: usize,
+    pub id: usize,
     #[serde(rename = "mintAddress")]
-    mint_address: String,
+    pub mint_address: String,
     #[serde(rename = "txSignature")]
-    tx_signature: String,
+    pub tx_signature: String,
     #[serde(rename = "fromWalletAddress")]
-    from_wallet_address: Option<String>,
+    pub from_wallet_address: Option<String>,
     #[serde(rename = "toWalletAddress")]
-    to_wallet_address: String,
-    amount: f32,
+    pub to_wallet_address: String,
+    pub amount: f32,
     #[serde(rename = "blockTime")]
-    block_time: String,
-    slot: usize,
+    pub block_time: String,
+    pub slot: usize,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SolanaNFTAuctionActivities {
     #[serde(rename = "mintAddress")]
-    mint_address: String,
+    pub mint_address: String,
 
     #[serde(rename = "auctionActivities")]
-    auction_activities: Vec<SolanaNFTAuctionActivity>,
+    pub auction_activities: Vec<SolanaNFTAuctionActivity>,
 
     #[serde(rename = "tokenTransfers")]
-    token_transfers: Vec<SolanaNFTTransfersEntity>,
+    pub token_transfers: Vec<SolanaNFTTransfersEntity>,
 }
 
 impl Marketplace {
-    fn new(api_key: String, env: NET_ENV, token: String) -> Marketplace {
-        Marketplace { api_key: api_key,  net: env, token: token}
+    pub fn new(api_key: String, env: NET_ENV, token: String) -> Marketplace {
+        Marketplace { api_key,  net: env, token }
     }
 
     // Create Verified Collection
-     async fn create_collection(&self, name: String, symbol: String, metadata_uri: String) -> Result<Option<CreateCollectionData>, Box<dyn Error>> {
+     pub async fn create_collection(&self, name: String, symbol: String, metadata_uri: String) -> Result<Option<CreateCollectionData>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -222,7 +212,7 @@ impl Marketplace {
     }
 
     // create verified sub collection
-    async fn create_sub_collection(&self, name: String, symbol: String, metadata_uri: String, parent_coll: String) -> Result<Option<CreateCollectionData>, Box<dyn Error>> {
+    pub async fn create_sub_collection(&self, name: String, symbol: String, metadata_uri: String, parent_coll: String) -> Result<Option<CreateCollectionData>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -234,14 +224,16 @@ impl Marketplace {
         data.insert("symbol", symbol);
         data.insert("url", metadata_uri);
         data.insert("collection_mint", parent_coll); // parent collection address
+        data.insert("confirmation", "confirmed".to_string());
 
         let response = client.post(url).headers(headers).json(&data).send().await?;
         let response_data = response.json::<Response<CreateCollectionData>>().await?;
+        println!("{:#?}", response_data);
         Ok(response_data.data)
     }
 
     // Mint NFT into collection
-    async fn solana_mint_nft(&self, payload: GeneralPayload) -> Result<Option<SolanaMintNftResult>, Box<dyn Error>> {
+    pub async fn solana_mint_nft(&self, payload: GeneralPayload) -> Result<Option<SolanaMintNftResult>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -256,7 +248,7 @@ impl Marketplace {
     }
 
     // List NFT ion Mirror World Marketplace
-    async fn listing_nft(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
+    pub async fn listing_nft(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -273,7 +265,7 @@ impl Marketplace {
     }
 
     // Purchase NFT on Mirror World Marketplace
-    async fn buy_nft(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
+    pub async fn buy_nft(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -290,7 +282,7 @@ impl Marketplace {
     }
 
     // Update NFT Listing on Mirror World Marketplace
-    async fn update_nft_listing(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
+    pub async fn update_nft_listing(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -307,7 +299,7 @@ impl Marketplace {
     }
 
     // Cancel listing NFT on Mirror World Marketplace
-    async fn cancel_nft_listing(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
+    pub async fn cancel_nft_listing(&self, mint_address: String, price: f64) -> Result<Option<NftListing>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -324,7 +316,7 @@ impl Marketplace {
     }
 
     // Transfer NFT from holder's wallet to another address
-    async fn transfer_nft(&self, mint_address: String, to_wallet_address: String) -> Result<Option<NftListing>, Box<dyn Error>> {
+    pub async fn transfer_nft(&self, mint_address: String, to_wallet_address: String) -> Result<Option<NftListing>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -337,11 +329,12 @@ impl Marketplace {
         })).send().await?;
 
         let response_data = response.json::<Response<NftListing>>().await?;
+        println!("response: {:?}", response_data);
         Ok(response_data.data)
     }
 
     // Fetch NFTs By Mint Addresses. Returns a detailed payload of all NFTs whose `mintAddresses`
-    async fn fetch_nfts_by_mint_address(&self, mint_address: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
+    pub async fn fetch_nfts_by_mint_address(&self, mint_address: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -355,12 +348,12 @@ impl Marketplace {
         })).send().await?;
 
         let response_data = response.json::<Response<SolanaNFTExtended>>().await?;
-
+        println!("{:#?}", response_data);
         Ok(response_data.data)
     }
 
     // Fetch NFTs By Creator Addresses. Returns a detailed payload of all NFTs whose `creatorAddresses`
-    async fn fetch_nfts_by_creator_address(&self, creators: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
+    pub async fn fetch_nfts_by_creator_address(&self, creators: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -379,7 +372,7 @@ impl Marketplace {
     }
 
     // Fetch NFTs By Update Authorities Addresses. Returns a detailed payload of all NFTs whose `updateAuthorities`
-    async fn fetch_nfts_by_update_authorities(&self, update_authorities: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
+    pub async fn fetch_nfts_by_update_authorities(&self, update_authorities: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -398,7 +391,7 @@ impl Marketplace {
     }
 
     // Fetch NFTs By Owners Addresses. Returns a detailed payload of all NFTs whose `owners`
-    async fn fetch_nfts_by_owner_addresses(&self, addresses: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
+    pub async fn fetch_nfts_by_owner_addresses(&self, addresses: Vec<String>, limit: usize, offset: usize) -> Result<Option<SolanaNFTExtended>, Box<dyn Error>> {
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -417,7 +410,7 @@ impl Marketplace {
     }
 
     // Fetch Solana NFT Marketplace Activity
-    async fn fetch_nft_marketplace_activity(&self, mint_address: String) -> Result<Option<SolanaNFTAuctionActivities>, Box<dyn Error>>{
+    pub async fn fetch_nft_marketplace_activity(&self, mint_address: String) -> Result<Option<SolanaNFTAuctionActivities>, Box<dyn Error>>{
         let headers:HeaderMap = get_request_header(self.api_key.to_string(), self.token.to_string());
 
         let client = Client::new();
@@ -441,19 +434,3 @@ impl Marketplace {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{Marketplace, NET_ENV};
-
-    #[tokio::test]
-     async fn test_create_collection() {
-        let key = "dvriNJlG7ITz1q0S57ZBOAWaDzA3cfjjcnU";
-        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTk5NiwiZXRoX2FkZHJlc3MiOiJHMWllaTNCV2dSWnNTQjNSaHJzRHdHR2p5QWpyTXpKeUZaWlpuMXQyM3lteCIsInNvbF9hZGRyZXNzIjoiRzFpZWkzQldnUlpzU0IzUmhyc0R3R0dqeUFqck16SnlGWlpabjF0MjN5bXgiLCJlbWFpbCI6ImxpdXlhbmdAcmN0LnN0dWRpbyIsIndhbGxldCI6eyJldGhfYWRkcmVzcyI6IjB4QzhjYTREOTY2OURCQWIzRTBERDI4QmZEMzhhYjdBMTgyN2VDNmNjMyIsInNvbF9hZGRyZXNzIjoiRzFpZWkzQldnUlpzU0IzUmhyc0R3R0dqeUFqck16SnlGWlpabjF0MjN5bXgifSwiY2xpZW50X2lkIjpudWxsLCJpYXQiOjE2NjEzOTY4NzgsImV4cCI6MTY2Mzk4ODg3OCwianRpIjoiYXV0aDo1OTk2In0.DU6-drhby1g3jF4eQm0OILoYQedDc1x7avY_Kpzn2QU";
-    
-        let m = Marketplace::new(key.to_string(), NET_ENV::DEVNET, token.to_string());
-
-        let response = m.create_collection("TEST_ASSERT_0825_2".to_string(), "NN".to_string(), "https://market-assets.mirrorworld.fun/gen1/1.json".to_string()).await.unwrap();
-        // println!("{:#?}", response == None);
-        // assert_eq!(response.name, "TEST_ASSERT_0825_1".to_string())
-    }
-}
